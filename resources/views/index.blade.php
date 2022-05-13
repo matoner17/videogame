110a67229af5834e17fa06320da764de7b6cdd49
= <=>
2022-05-13 13:57:49 -0400

    Style the Index Page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container mx-auto px-4">
+        <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
+            Popular Games
+        </h2>
+        <div class="popular-games grid grid-cols-6 gap-12 text-sm border-b border-gray-800 pb-16">
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="/ff7.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-16 -right-5 w-16 h-16 bg-gray-800 rounded-full">
+                        <div class="flex justify-center font-semibold text-xs items-center h-full">
+                            80%
+                        </div>
+                    </div>
+                    <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                        Final Fantasy VII Remake
+                    </a>
+                    <div class="text-gray-400 mt-1">
+                        Playstation 4
+                    </div>
+                </div>
+            </div>
+        </div> <!-- end popular games -->
+        <div class="flex my-10">
+            <div class="recently-reviewed w-3/4 mr-32">
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
+                    Recently Reviewed
+                </h2>
+                <div class="recently-reviewed-container space-y-12 mt-8">
+                    <div class="game flex bg-gray-800 rounded-lg shadow-md p-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img src="/ff7.jpg" alt="game cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-16 -right-5 w-16 h-16 bg-gray-900 rounded-full">
+                                <div class="flex justify-center font-semibold text-xs items-center h-full">
+                                    80%
+                                </div>
+                            </div>
+                            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                                Final Fantasy VII Remake
+                            </a>
+                            <div class="text-gray-400 mt-1">
+                                Playstation 4
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
+                                Final Fantasy VII Remake
+                            </a>
+                            <div class="text-gray-400 mt-1">
+                                Playstation 4
+                            </div>
+                            <p class="mt-6 text-gray-400">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex atque quaerat aliquam, doloribus ea possimus quod perferendis accusamus culpa id adipisci soluta, asperiores earum sequi velit dolorum distinctio, impedit corporis?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="most-anticipated w-1/4">
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
+                    Most Anticipated
+                </h2>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="flex game">
+                        <a href="#">
+                            <img src="/cyberpunk.jpg" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#">Cyberpunk 2077</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                        </div>
+                    </div>
+                </div>
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold mt-12">
+                    Coming Soon
+                </h2>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="flex game">
+                        <a href="#">
+                            <img src="/cyberpunk.jpg" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#">Cyberpunk 2077</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- end container -->
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Video Game Aggregator</title>
+</head>
+<body class="bg-gray-900 text-white">
+    <header class="border-b border-gray-800">
+        <nav class="flex items-center justify-between px-4 py-6 container mx-auto">
+            <div class="flex items-center">
+                <a href="/">
+                    <img src="/laracasts-logo.svg" alt="Laracasts Logo" class="flex-none w-32">
+                    <ul class="flex ml-16 space-x-8">
+                        <li><a href="#" class="hover:text-gray-400">Games</a></li>
+                        <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
+                        <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
+                    </ul>
+                </a>
+            </div>
+            <div class="flex items-center">
+                <div class="relative">
+                    <input
+                        type="text"
+                        class="bg-gray-800 text-sm rounded-full w-64 px-3 pl-8 py-1 focus:outline-none focus:shadow-outline"
+                        placeholder="Search..."
+                    >
+                    <div class="flex items-center h-full ml-2 absolute top-0">
+                        a
+                    </div>
+                </div>
+                <div class="ml-6">
+                    <a href="#">
+                        <img src="/avatar.jpg" alt="avatar" class="rounded-full w-8">
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main class="py-8">
+        @yield('content')
+    </main>
+
+    <footer class="border-t border-gray-800">
+        <div class="container mx-auto px-4 py-6">
+            Powered by <a href="#" class="underline hover:text-gray-400">IGDB API</a>
+        </div>
+    </footer>
+</body>
+</html>
