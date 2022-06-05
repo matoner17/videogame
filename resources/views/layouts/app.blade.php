a2b960ba4ffb23cf9c817ae41dbb9e02734a0240
Matthew Toner <matoner17@gmail.com>
2022-06-05 18:38:01 -0400

    Updated CSS link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,21 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
     @livewireStyles
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <title>@yield('pageTitle')</title>
 </head>
-<body class="bg-gray-900 text-white">
+<body class="text-white bg-gray-900">
     <header class="border-b border-gray-800">
-        <nav class="flex flex-col lg:flex-row items-center justify-between container px-4 py-6 mx-auto">
-            <div class="flex flex-col lg:flex-row items-center">
+        <nav class="container flex flex-col items-center justify-between px-4 py-6 mx-auto lg:flex-row">
+            <div class="flex flex-col items-center lg:flex-row">
                 <a href="/">
                     <img src="/laracasts-logo.svg" alt="Logo" class="flex-none w-32">
-                    <ul class="flex ml-0 mt-6 lg:ml-16 md:mt-0 space-x-8">
-                        {{-- <li><a href="#" class="hover:text-gray-400">Games</a></li>
-                        <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
-                        <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li> --}}
+                    <ul class="flex mt-6 ml-0 space-x-8 lg:ml-16 md:mt-0">
                     </ul>
                 </a>
             </div>
@@ -26,7 +23,7 @@
                 <livewire:search-dropdown>
                 <div class="ml-6">
                     <a href="#">
-                        <img src="/avatar.jpg" alt="avatar" class="rounded-full w-8">
+                        <img src="/avatar.jpg" alt="avatar" class="w-8 rounded-full">
                     </a>
                 </div>
             </div>
@@ -38,7 +35,7 @@
     </main>
 
     <footer class="border-t border-gray-800">
-        <div class="container mx-auto px-4 py-6">
+        <div class="container px-4 py-6 mx-auto">
             Powered by <a href="#" class="underline hover:text-gray-400">IGDB API</a>
         </div>
     </footer>
